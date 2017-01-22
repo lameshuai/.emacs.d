f08fa9305705563cc2779059b76bc3fb1cab66f5
Douglas <oneforalone@gmail.com>
2017-01-22 20:37:09 +0800

    update

--- site-lisp/init-clisp.el
+++ site-lisp/init-clisp.el
@@ -1,16 +1,9 @@
 ;;; init-clisp.el
 
-;; linuxs version
-
-
-
-
-;; widnows version
-
-(setq inferior-lisp-program "~/.emacs.d/sbcl/sbcl.exe")
+;; (setq inferior-lisp "/usr/bin/sbcl")	;; debian/ubuntu version
+;; (setq inferior-lisp-program "~/.emacs.d/sbcl/sbcl.exe") ;; windows version
 (slime-setup)
 (slime-setup '(slime-fancy))
-
 
 (provide 'init-clisp)
 
