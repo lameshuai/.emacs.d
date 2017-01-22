e23ae86fd92bdac1e2795a49e70dc2cb1b9882ab
Douglas <1910706030@qq.com>
2017-01-22 11:15:37 +0800

    add coding enviroment utf-8

--- site-lisp/init-custom.el
+++ site-lisp/init-custom.el
@@ -1,5 +1,20 @@
 ;;; init-custom.el
 
+;; code enviroment
+
+(set-terminal-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq-default pathname-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+
+
+;; bootup
 (ido-mode t)
 (tool-bar-mode -1)
 (show-paren-mode t)
