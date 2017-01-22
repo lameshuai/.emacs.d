3e57365efdae4f832cb20da9de9a57295562a302
Douglas <oneforalone@gmail.com>
2017-01-22 22:24:24 +0800

    fix slime

--- site-lisp/init-clisp.el
+++ site-lisp/init-clisp.el
@@ -1,9 +1,19 @@
 ;;; init-clisp.el
 
-;; (setq inferior-lisp "/usr/bin/sbcl")	;; debian/ubuntu version
-;; (setq inferior-lisp-program "~/.emacs.d/sbcl/sbcl.exe") ;; windows version
-(slime-setup)
-(slime-setup '(slime-fancy))
+(add-to-list 'load-path "~/.emacs.d/slime/")
+
+;; ;; debian/ubuntu version
+;; (setq inferior-lisp-program "/usr/bin/sbcl")
+;; (setq slime-contribs '(slime-fancy))
+
+;; ;; windows version
+;; (setq inferior-lisp-program "~/.emacs.d/sbcl/sbcl.exe")
+;; (slime-setup)
+;; (slime-setup '(slime-fancy))
+
+(require 'slime)
+(require 'slime-autoloads)
+
 
 (provide 'init-clisp)
 
