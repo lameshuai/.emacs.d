8971c3e2a96492212ce0b738a4c589282aa1f5c2
Douglas <oneforalone@gmail.com>
2017-01-22 20:13:34 +0800

    clisp

---
+++ site-lisp/init-clisp.el
@@ -0,0 +1,16 @@
+;;; init-clisp.el
+
+;; linuxs version
+
+
+
+
+;; widnows version
+
+(setq inferior-lisp-program "~/.emacs.d/sbcl/sbcl.exe")
+(slime-setup)
+(slime-setup '(slime-fancy))
+
+
+(provide 'init-clisp)
+
