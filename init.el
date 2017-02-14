b067338117d0cae20fa54461495551166ad2249b
Douglas <oneforalone@gmail.com>
2017-02-14 16:03:24 +0800

    Update some Interface

--- init.el
+++ init.el
@@ -1,31 +1,86 @@
-;;; init.el
 
-;;; This file bootstraps the configure-info-directory, which is devided into
+;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
 
+(let ((minver "23.3"))
+  (when (version<= emacs-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
-;; plugins location
-(add-to-list
- 'load-path
- (expand-file-name "site-lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
+;; (require 'init-benchmarking) ;; Measure startup time
 
-;; pluigns
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+;;----------------------------------------------------------------------------
+;; Temporarily reduce garbage collection during startup
+;;----------------------------------------------------------------------------
+(defconst sanityinc/initial-gc-cons-threshold gc-cons-threshold
+  "Initial value of `gc-cons-threshold' at start-up time.")
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'after-init-hook
+          (lambda () (setq gc-cons-threshold sanityinc/initial-gc-cons-threshold)))
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; (require 'init-compat)
 (require 'init-utils)
-(require 'init-site-lisp)
-(require 'init-elpa)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;; Calls (package-initialize)
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-preload-local.el"
+;;----------------------------------------------------------------------------
+;; (require 'init-preload-local nil t)
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific features 
+;;----------------------------------------------------------------------------
+
+
+(require-package 'diminish)
+
 (require 'init-custom)
 (require 'init-company)
 (require 'init-ido)
 (require 'init-smex)
 (require 'init-smartparens)
-(require 'init-python)
-(require 'init-matlab)
-(require 'init-counsel-gtags)
+(require 'init-window)
+(require 'init-editing-utils)
 (require 'init-theme)
 (require 'init-powerline)
-(require 'init-window)
-(require 'init-info)
+
+
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific modes
+;;----------------------------------------------------------------------------
+
+
+;; (require 'init-python)
+;; (require 'init-matlab)
+;; (require 'init-counsel-gtags)
 ;; (require 'init-clisp)
 
 
+;;============================================================================
+
+;; My own informations display when startup
+(require 'init-info)
+
+;;============================================================================
+
+
 (provide 'init)
+
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
