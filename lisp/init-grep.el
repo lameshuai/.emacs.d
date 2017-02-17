00700bb0abd12b29ee26fcdc6f1385a3fe384f85
Douglas <oneforalone@gmail.com>
2017-02-17 10:13:14 +0800

    add grep & ivy

---
+++ lisp/init-grep.el
@@ -0,0 +1,16 @@
+;;; init-grep.el
+
+(setq-default grep-highlight-matches t
+              grep-scroll-output t)
+
+(when *is-a-mac*
+  (setq-default locate-command "mfind"))
+
+(when (executable-find "ag")
+  (require-package 'ag)
+  (require-package 'wgrep-ag)
+  (setq-default ag-highlight-search t)
+  (global-set-key (kbd "M-?") 'ag-project))
+
+
+(provide 'init-grep)
